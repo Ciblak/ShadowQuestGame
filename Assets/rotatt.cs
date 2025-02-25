49a6804d4576cccd9b7163e00d4026036c541a2e
Ciblak <cagdas.arasc@gmail.com>
2025-02-25 22:17:43 +0100

    Few animation tweaks

--- Assets/rotatt.cs
+++ Assets/rotatt.cs
@@ -1,31 +1,40 @@
 using UnityEngine;
 
-public class RotateAndScaleState : StateMachineBehaviour
+public class rotatt : StateMachineBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     private Transform objTransform;
+    private Color originalColor = Color.white;
     private Vector3 originalScale;
-    private Quaternion originalRotation;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         objTransform = animator.transform;
+        spriteRenderer = animator.GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            originalColor = spriteRenderer.color;
+            spriteRenderer.color = new Color(0f, 0f, 0f, 0.5f);
+        }
 
         if (objTransform != null)
         {
             originalScale = objTransform.localScale;
-            originalRotation = objTransform.rotation;
-
-            objTransform.Rotate(0, 0, -90);
-            objTransform.localScale *= 3f;
+            objTransform.localScale = originalScale * 0.75f;
         }
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = originalColor;
+        }
+
         if (objTransform != null)
         {
             objTransform.localScale = originalScale;
-            objTransform.rotation = originalRotation;
         }
     }
 }
